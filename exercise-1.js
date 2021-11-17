52f607e89bab5baf1de5802874b029b16d9be76a
Gnam <giuseppemangiavillano1@gmail.com>
2021-11-17 20:38:55 +0100

    first commit

---
+++ exercise-1.js
@@ -0,0 +1,15 @@
+
+
+function canPlay() {
+  let personName;
+  
+  if (true) {
+     personName = 'Paul';
+  }
+
+  personName += ' plays football'; // personName è stato definito con let
+
+  console.log(personName);
+}
+
+canPlay();
